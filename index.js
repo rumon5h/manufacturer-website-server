5813c0e0e7f1cf644512329b15081a587bf3d235
rumon5 <mdrumonkhan48519@gmail.com>
2022-05-23 21:10:46 +0600

    Connected mongodb

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ck7ho.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        client.connect();
+        console.log('MONGODB connected.');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+});
