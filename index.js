1d51654838add147eb30bed12c33255d2c46c758
rumon5 <mdrumonkhan48519@gmail.com>
2022-05-27 12:01:26 +0600

    JWT implementation done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -12,6 +13,23 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ck7ho.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'UnAuthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 
 async function run() {
     try {
@@ -25,6 +43,17 @@ async function run() {
         // PUT AUTH USER 
         app.put('/authUser/:email', async(req, res) =>{
             const email = req.params.email;
+            const filter = {email: email};
+            const options = { upsert: true };
+            const data = req.body;
+            const updateDoc = {
+                $set: data
+              };
+          const result = await usersAuthCollection.updateOne(filter, updateDoc, options);
+          const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN, {expiresIn: '2h'})
+          console.log(token);
+          console.log(process.env.ACCESS_TOKEN);
+          res.send({result, token});
 
         })
 
